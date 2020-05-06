665f270aba370034be4fe1f570f9defaa2b9b451
SHAIK JAFAR SHARIF <jafarsharif6@gmail.com>
2020-05-07 01:58:42 +0530

    Add files via upload

---
+++ main_test.py
@@ -0,0 +1,20 @@
+from unittest.mock import Mock
+
+import main
+
+
+def test_clock_angle():
+    name = 'test'
+    data = {'hour': 4,'minute':45}
+    req = Mock(get_json=Mock(return_value=data), args=data)
+
+    # Call tested function
+    assert main.calcAngle(req) == 'Angle is {}!'.format(angle)
+
+
+def test_print_hello_world():
+    data = {}
+    req = Mock(get_json=Mock(return_value=data), args=data)
+
+    # Call tested function
+    assert main.calcAngle(req) == 'Angle is {}'.format(angle)
