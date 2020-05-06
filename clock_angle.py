a8dbf238faf7f01dd2fff8d75d70ced2d59166b8
SHAIK JAFAR SHARIF <jafarsharif6@gmail.com>
2020-05-07 01:35:49 +0530

    Update clock_angle.py

--- clock_angle.py
+++ clock_angle.py
@@ -1,17 +1,25 @@
-def calcAngle(h,m):
+def calcAngle(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
 
-    if (h < 0 or m < 0 or h > 12 or m > 60):
+    if request_json and ('hour' in request_json and 'minute' in request_json):
+        hour = request_json['hour']
+        minute = request_json['minute']
+    else:
+        hour = 12
+        minute = 0
+    if (hour < 0 or minute < 0 or hour > 12 or minute > 60):
         print('Wrong input')
-    if (h == 12):
-        h = 0
-    if (m == 60):
-        m = 0
+    if (hour == 12):
+        hour = 0
+    if (minute == 60):
+        minute = 0
 
     # Calculate the angles moved by
     # hour and minute hands with
     # reference to 12:00
-    hour_angle = 0.5 * (h * 60 + m)
-    minute_angle = 6 * m
+    hour_angle = 0.5 * (hour * 60 + minute)
+    minute_angle = 6 * minute
 
     # Find the difference between two angles
     angle = abs(hour_angle - minute_angle)
@@ -20,4 +28,4 @@ def calcAngle(h,m):
     # possible angles
     angle = min(360 - angle, angle)
 
-    return angle
+    return str(int(angle))
