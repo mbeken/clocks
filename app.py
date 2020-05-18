0fed54cf0c74ff39a4d8f44411093d1d1708f9c1
Thamizhathena <thamizh.dm@hotmail.com>
2020-05-18 16:22:09 +0530

    space removed

--- app.py
+++ app.py
@@ -31,5 +31,4 @@ def home():
     return render_template("Home.html", pageData=json.dumps(answer))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))  
-    
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
