52c3c1ddfdd9d53293b8ef3f43fc2289df370a1e
Deepikakolandasamy <deepikakece@gmail.com>
2020-05-18 17:54:20 +0530

    modified code

--- app.py
+++ app.py
@@ -9,23 +9,33 @@ import os
 from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='flask_template')
 OUTPUT = "e"
-@app.route('/', methods=['GET'])
-def home():
-    """Go to the HTML template"""
-    return render_template('angle.html', message=OUTPUT)
-@app.route('/user', methods=['POST'])
-def reset():
-    """ Calculate the clock function """
+def func1(hour, minute):
+    """ Function passed from reset."""
     try:
-        hour = int(request.form['hour'])
-        minute = int(request.form['minute'])
         if ((hour >= 1) & (hour <= 12)) & ((minute >= 0) & (minute <= 60)):
             angle = abs((hour * 30 + minute * 0.5)-(minute * 6))
             if angle > 180:
                 angle = 360 - angle
             msg = "Angle between %s hours and %s minutes : "%(hour, minute)+str(angle)
+            print(msg)
         else:
             msg = "Enter correct integer value(Hour=1 to 12, Minute=0 to 60)"
+        return msg
+    except ValueError:
+        msg = "Enter correct integer value(Hour=1 to 12, Minute=0 to 60)"
+        return msg
+@app.route('/', methods=['GET'])
+def home():
+    """Go to the HTML template"""
+    return render_template('angle.html', message=OUTPUT)
+@app.route('/user', methods=['GET', 'POST'])
+def reset():
+    """ Calculate the clock function """
+    try:
+        hour = int(request.form['hour'])
+        minute = int(request.form['minute'])
+        msg = func1(hour, minute)
+        print(msg)
         return render_template('angle.html', message=msg)
     except ValueError:
         msg = "Enter correct integer value(Hour=1 to 12, Minute=0 to 60)"
