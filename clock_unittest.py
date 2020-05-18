113ba8a688fbf5f8c7c5a363a1af2df83290287c
Deepikakolandasamy <deepikakece@gmail.com>
2020-05-18 17:57:36 +0530

    modified test case

--- clock_unittest.py
+++ clock_unittest.py
@@ -1,33 +1,54 @@
-"""Unit test
-"""
+
 # coding: utf-8
 
-# In[14]:
+# In[16]:
 
-import unittest
-class Testing(unittest.TestCase):
-    def test_1(self):
-        try:
-            hour = int(3)
-            minute = int(0)
-            if ((hour >= 1) & (hour <= 12)) & ((minute >= 0) & (minute <= 60)):
-                angle = abs((hour * 30 + minute * 0.5)-(minute * 6))
-            else:
-                angle = "Enter correct interger value"
-        except ValueError:
-            angle = "Enter correct interger value"
-        self.assertEqual(angle, 90)
-    def test_3(self):
-        hour = 13
-        minute = 50
-        try:
-            if ((hour >= 1) & (hour <= 12)) & ((minute >= 0) & (minute <= 60)):
-                angle = abs((hour * 30 + minute * 0.5)-(minute * 6))
-            else:
-                angle = "Enter correct interger value"
-        except ValueError:
-            angle = "Enter correct interger value"
-        self.assertEqual(angle, "Enter correct interger value")
-if __name__ == '__main__': 
-    unittest.main()
-    
+from  app import func1
+def test_1():
+    """
+        Testing at 3 hrs 0 mins - Angle = 90
+        """
+    hour = 3
+    minute = 0
+    angle1 = func1(hour,minute)
+    try:
+        angle1 = float(angle1.split(':')[1])
+    except:
+        None
+    assert angle1 == 90.0
+def test_2():
+    """
+        Testing at 12 hrs 50 mins - Angle = 85
+        """
+    hour = 12
+    minute = 50
+    angle1 = func1(hour,minute)
+    try:
+        angle1 = float(angle1.split(':')[1])
+    except:
+        None
+    assert angle1 == 85.0
+def test_3():
+    """
+        Testing at 23 hrs 50 mins - Angle = "Enter correct integer value(Hour=1 to 12, Minute=0 to 60)"
+        """
+    hour = 23
+    minute = 50
+    angle1 = func1(hour,minute)
+    try:
+        angle1 = float(angle1.split(':')[1])
+    except:
+        None
+    assert angle1 == "Enter correct integer value(Hour=1 to 12, Minute=0 to 60)"
+def test_4():
+    """
+        Testing at empty string - Angle = "Enter correct integer value(Hour=1 to 12, Minute=0 to 60)"
+        """
+    hour = ""
+    minute = ""
+    angle1 = func1(hour,minute)
+    try:
+        angle1 = float(angle1.split(':')[1])
+    except:
+        None
+    assert angle1 == "Enter correct integer value(Hour=1 to 12, Minute=0 to 60)"
