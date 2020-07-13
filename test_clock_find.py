55577a7bddec3e3ebec787ebb4db7d0f6900c015
Saloni Soniminde <saloni.soniminde@harman.com>
2020-07-13 14:06:43 +0530

    made some minor changes

--- test_clock_find.py
+++ test_clock_find.py
@@ -7,29 +7,29 @@ class Test(unittest.TestCase):
         self.app_context.push()
 
     def test_clock_negative_testing(self):
-        data=calc_angle(26,30)
+        data=calc_angle("26:30")
         expected_data= "Invalid hour given"
         self.assertEqual(expected_data,data)
 
-        data=calc_angle(12,67)
+        data=calc_angle("12:67")
         expected_data = "Minute should be in range of  0 to 59"
         self.assertEqual(expected_data, data)
 
-        data = calc_angle(-2, 67)
+        data = calc_angle("-2:67")
         expected_data = "Minute should be in range of  0 to 59"
         self.assertEqual(expected_data, data)
 
-        data = calc_angle(-2, -67)
+        data = calc_angle("-2:-67")
         expected_data = "Minute should be in range of  0 to 59"
         self.assertEqual(expected_data, data)
 
     def test_clock_positive_testing(self):
-        data=calc_angle(2,20)
-        expected_data= 50
+        data=calc_angle("2:20")
+        expected_data="Angle for time is 50.0 degree."
         self.assertEqual(expected_data, data)
 
-        data=calc_angle(22,20)
-        expected_data= 170
+        data=calc_angle("22:20")
+        expected_data= "Angle for time is 170.0 degree."
         self.assertEqual(expected_data, data)
 
 if __name__ == "__main__":
